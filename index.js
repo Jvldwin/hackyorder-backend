1702882b9bc2ea3f0a5e3244b1211d4dc905d689
jvldwin <Mathematiciannguyen@gmail.com>
2022-02-16 23:30:35 +0700

    Added Cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+import cors from 'cors'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
