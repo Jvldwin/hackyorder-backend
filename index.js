01912ce6ff02e4f32a078c12887cd785ef6e7b9c
jvldwin <Mathematiciannguyen@gmail.com>
2022-02-20 16:51:19 +0700

    Added sample data

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-  res.send('First post')
+  res.send([
+    {
+      id: '1',
+      title: 'First Post!',
+      content: 'Hello!',
+      user: '0',
+      date: Date('1995-12-17T03:24:00'),
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0,
+      },
+    },
+    {
+      id: '2',
+      title: 'Second Post',
+      content: 'More text',
+      user: '2',
+      date: Date('1995-12-17T03:24:00'),
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0,
+      },
+    },
+  ])
 })
 
 app.listen(port, () => {
