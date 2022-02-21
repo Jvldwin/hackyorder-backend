0a4126f515936febc18dfccdccf455f3b3ce65ec
jvldwin <Mathematiciannguyen@gmail.com>
2022-02-21 13:23:36 +0700

    Added post route

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const  cors = require('cors')
+const { json } = require('express/lib/response')
 
 app.use(cors())
+app.use(json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
